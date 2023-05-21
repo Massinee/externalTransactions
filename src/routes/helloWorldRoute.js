532518df103df68e164ef403296e90af855bc0d1
mariana.massine <mariana.massine@bemobi.com.br>
2023-05-21 18:47:26 -0300

    run sonarcloud

--- src/routes/helloWorldRoute.js
+++ src/routes/helloWorldRoute.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {handler} = require('../index');
+const { handler } = require('../index');
 
 const routes = async (fastify) => {
     fastify.get('/helloWorld', (request, reply) => {
