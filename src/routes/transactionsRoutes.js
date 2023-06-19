1dfaf5186fdbab7a88e9841a53d6c373befd7bf2
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-19 20:52:29 -0300

    remove todo

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -5,16 +5,12 @@ const emailService = require('../services/enviarEmailService');
 const validaCartaoSchema = require('./schemas/validaCartaoSchema');
 const validaCartaoService = require('../services/validaCartaoService');
 
-//Todo:
-//ver o que tem que fixar exposto ou nao
 //POST /enviarEmail -done
 //POST /cobranca
 //POST /processaCobrancasEmFila - tem cron no cyclic!!
 //POST /filaCobranca
 //GET /cobranca/{idCobranca}
 //POST /validaCartaoDeCredito - done
-
-
 
 const routes = async (fastify) => {
     fastify.post('/enviarEmail', emailSchema, emailService.enviarEmail);
