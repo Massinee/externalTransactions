990f9d0941c71f49d91f3cb16191e121e5c67d91
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 12:35:39 -0300

    TENTANDO FAZER O CRON ACEITAR CORPO VAZIO

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 'use strict'
 
 const fastify = require('fastify');
-const fastifyRawBody = require('fastify-raw-body');
-
 
 const build = (opts = {}) => {
     const app = fastify(opts);
 
-    app.register(fastifyRawBody, {
-        global: false, // Desabilitar o parsing global de corpo
-        runFirst: true, // Executar antes de outros handlers
-        parse: (request, _payload, done) => {
-            // Definir o corpo como null quando estiver vazio
-            if (_payload.length === 0) {
-                request.body = null;
+    app.addContentTypeParser('*', (req, done) => {
+        let data = '';
+
+        req.on('data', chunk => {
+            data += chunk;
+        });
+
+        req.on('end', () => {
+            // Verificar se o corpo está vazio
+            if (data.length === 0) {
+                req.body = null;
+            } else {
+                try {
+                    req.body = JSON.parse(data);
+                } catch (error) {
+                    return done(error);
+                }
             }
 
             done();
-        },
-    })
+        });
+    });
     app.register(require('./routes/helloWorldRoute'));
     app.register(require('./routes/transactionsRoutes'));
 
