7c9e3b56a4bf10ccf68cbb865702dfa1c0ccdd30
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 13:49:06 -0300

    TA RODNANDO AMEM

--- src/cronProcessarFila.js
+++ src/cronProcessarFila.js
@@ -5,10 +5,10 @@ const { build } = require('../src/app');
 const app = build();
 const log = require('../src/utils/logUtils');
 const axios = require("axios");
-const CRON = process.env.CRON || '*/20 * * * * *';
+const CRON = process.env.CRON || '0 */12 * * *';
 
 const runCron = () => {
-    return cron.scheduleJob('* * * * *', () => {
+    return cron.scheduleJob(CRON, () => {
         callProcessarFila().then(r => log.info('Fila de cobranças processada')).catch(err => console.log(err));
     });
 };
