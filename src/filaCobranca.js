b93c209d14707eea81e96f56bc5d161a5119a1c8
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-27 22:39:35 -0300

    fix: tinha colocado modelo errado no banco

--- src/filaCobranca.js
+++ src/filaCobranca.js
@@ -1,26 +1,14 @@
 const devedores = [
     {
-        "id": "b46d0649-1a5c-4489-86d8-2a8434e9e4e4",
-        "status": "FALHA",
-        "horaSolicitacao": "2023-06-28T00:19:45.384Z",
-        "horaFinalizacao": "2023-06-28T00:19:48.146Z",
+        "valor": "25",
+        "ciclista": "13"
+    },
+    {
         "valor": "10",
         "ciclista": "1"
     },
     {
-        "id": "e833c410-7273-41d3-834e-5f16effd4466",
-        "status": "FALHA",
-        "horaSolicitacao": "2023-06-28T00:19:45.384Z",
-        "horaFinalizacao": "2023-06-28T00:19:48.146Z",
-        "valor": "10",
-        "ciclista": "2"
-    },
-    {
-        "id": "a51a7021-d567-45aa-a6fb-38d81f95c08a",
-        "status": "FALHA",
-        "horaSolicitacao": "2023-06-28T00:19:45.384Z",
-        "horaFinalizacao": "2023-06-28T00:19:48.146Z",
-        "valor": "10",
+        "valor": "5",
         "ciclista": "4"
     }
 ];
--- src/services/filaCobrancaService.js
+++ src/services/filaCobrancaService.js
@@ -4,12 +4,13 @@ const log = require("../utils/logUtils");
 const { buildResponse } = require('../services/realizarCobrancaService');
 const { devedores } = require('../filaCobranca');
 
-const filaCobranca = async (valor, ciclistaId) => {
+const filaCobranca = async (valor, ciclista) => {
     log.info("Iniciando a função para incluir cobrança na fila");
     try {
         const horaSolicitacao = new Date().toISOString();
-        const message = await buildResponse("FALHA", valor, ciclistaId, horaSolicitacao);
-        devedores.push(message);
+        const message = await buildResponse("FALHA", valor, ciclista, horaSolicitacao);
+        devedores.push({valor, ciclista});
+        console.log("$$$$$$$$$$$$$$$$", devedores)
 
         return { statusCode: 200, message };
 
