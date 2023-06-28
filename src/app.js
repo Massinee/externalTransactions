a65a34d40392e26d6f6a5e2af575a742e3e27493
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 12:46:18 -0300

    mais uma tentativa

--- src/app.js
+++ src/app.js
@@ -1,32 +1,12 @@
 'use strict'
 
 const fastify = require('fastify');
+const formBody = require('fastify-formbody');
 
 const build = (opts = {}) => {
     const app = fastify(opts);
 
-    app.addContentTypeParser('*', (req, done) => {
-        let data = '';
-
-        req.on('data', chunk => {
-            data += chunk;
-        });
-
-        req.on('end', () => {
-            // Verificar se o corpo está vazio
-            if (data.length === 0) {
-                req.body = null;
-            } else {
-                try {
-                    req.body = JSON.parse(data);
-                } catch (error) {
-                    return done(error);
-                }
-            }
-
-            done();
-        });
-    });
+    app.register(formBody);
     app.register(require('./routes/helloWorldRoute'));
     app.register(require('./routes/transactionsRoutes'));
 
