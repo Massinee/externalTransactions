8f617b04c48f7a77f600775bd29ff7a279654823
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 11:47:52 -0300

    fastify raw body

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 'use strict'
 
 const fastify = require('fastify');
-const accepts = require('@fastify/accepts');
+const fastifyRawBody = require('fastify-raw-body');
+
 
 const build = (opts = {}) => {
     const app = fastify(opts);
 
-    app.register(accepts);
+    app.register(fastifyRawBody, {
+        global: false, // Desabilitar o parsing global de corpo
+        runFirst: true, // Executar antes de outros handlers
+        parse: (request, _payload, done) => {
+            // Definir o corpo como null quando estiver vazio
+            if (_payload.length === 0) {
+                request.body = null;
+            }
+
+            done();
+        },
+    })
     app.register(require('./routes/helloWorldRoute'));
     app.register(require('./routes/transactionsRoutes'));
 
