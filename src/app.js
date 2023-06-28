1022bf623e795dca68ed5a7c5dc09441c5904401
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 12:52:31 -0300

    pelo amor de deus

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const fastify = require('fastify');
-const formBody = require('fastify-formbody');
+const formBody = require('@fastify/formbody');
 
 const build = (opts = {}) => {
     const app = fastify(opts);
 
-    app.register(formBody);
+    app.register(formBody, {
+        bodyLimit: 0, // Configurar limite do corpo como 0 para permitir corpos vazios
+    });
     app.register(require('./routes/helloWorldRoute'));
     app.register(require('./routes/transactionsRoutes'));
 
