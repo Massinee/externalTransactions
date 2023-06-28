3127d0ef37307b73c347f95efab8f109b1d83090
mariana.massine <mariana.massine@bemobi.com.br>
2023-06-28 13:45:42 -0300

    vamo ve se ta rodando msm

--- src/cronProcessarFila.js
+++ src/cronProcessarFila.js
@@ -4,10 +4,11 @@ const cron = require('node-schedule');
 const { build } = require('../src/app');
 const app = build();
 const log = require('../src/utils/logUtils');
-const CRON = process.env.CRON || '*/5 * * * *';
+const axios = require("axios");
+const CRON = process.env.CRON || '*/20 * * * * *';
 
 const runCron = () => {
-    return cron.scheduleJob(CRON, () => {
+    return cron.scheduleJob('* * * * *', () => {
         callProcessarFila().then(r => log.info('Fila de cobranças processada')).catch(err => console.log(err));
     });
 };
